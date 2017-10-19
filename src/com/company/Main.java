8d5bd83451825ad30dd2c121e1af62f0aedc2885
manderson18 <ma128@pvsd.org>
2017-10-19 09:00:45 -0400

    ass

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,47 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+
+//int grades[] = {90,95,100};
+
+       // double average = (grades[0] + grades[1] + grades[2]) / 3;
+       // System.out.println(average); // Challenge 1
+
+
+
+
+
+        double userGrades[] = new double[2000000];
+
+        Scanner kbin = new Scanner(System.in);
+
+        int i= 0;
+
+        for(i=0;i < userGrades.length; i++ ){
+            System.out.println("Please enter a Grade");
+
+            int arrayInput = kbin.nextInt();
+
+            userGrades[i] = arrayInput;
+            System.out.println(arrayInput/i+1);
+
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
